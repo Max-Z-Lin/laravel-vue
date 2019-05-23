ced7259d2554a532a03c83b3863a1f1e394b987f
Max Lin <max_lin@owlting.com>
2019-05-23 16:11:15 +0800

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-navigation-drawer app></v-navigation-drawer>
+        <v-toolbar app></v-toolbar>
+        <v-content>
+            <v-container fluid >
+                <router-link tag="li" style="color:black;" to="/" >home</router-link>
+                <router-link tag="li" to="/about">about</router-link>
+
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+        <v-footer app></v-footer>
+    </v-app>
+</template>
+
+<script>
+    export default {}
+</script>
